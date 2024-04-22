4863f79e40f047aa48bab5cfbe8c0eab7cd4ae33
damanjotDev <damanjot@devronins.com>
2024-04-22 13:48:20 +0530

    add our team page

--- src/pages/landing/our_team.tsx
+++ src/pages/landing/our_team.tsx
@@ -1,14 +1,88 @@
 import React from 'react';
 import { Navbar } from '../../components/navbars/navbar'
+import { motion } from "../../utils/animation"
 import contactBackgroudImage from "../../assets/images/conatctBackground.png"
 import { TypographyH1, TypographyH4, TypographyH5, TypographyP } from '../../components/ui/Typography';
 import { IoLocation, MdKeyboardDoubleArrowRight } from "../../utils/icons"
 import { RoutesName } from '../../utils/constant';
 import { useNavigate } from 'react-router-dom';
 import Footer from '../../components/footer/footer';
+import HeroImage from "../../assets/images/hero.png"
+import { useSocialLinkRoutes } from '../../hooks/useSocialLinkRoutes';
+
+
 
 export const LandingOurTeam = () => {
   const navigate = useNavigate();
+
+  const teamMembers = [
+    {
+      id: '1',
+      title: "Founder & CEO",
+      name: 'Ashish Sudra',
+      description: "Lead the team of passionate developers, designers and the strategists with a lot of thought and analysis come true!",
+      social_links: useSocialLinkRoutes(),
+      imageUrl: HeroImage
+    },
+    {
+      id: '2',
+      title: "Founder & CEO",
+      name: 'Ashish Sudra',
+      description: "Lead the team of passionate developers, designers and the strategists with a lot of thought and analysis come true!",
+      social_links: useSocialLinkRoutes(),
+      imageUrl: HeroImage
+    },
+    {
+      id: '3',
+      title: "Founder & CEO",
+      name: 'Ashish Sudra',
+      description: "Lead the team of passionate developers, designers and the strategists with a lot of thought and analysis come true!",
+      social_links: useSocialLinkRoutes(),
+      imageUrl: HeroImage
+    },
+    {
+      id: '4',
+      title: "Founder & CEO",
+      name: 'Ashish Sudra',
+      description: "Lead the team of passionate developers, designers and the strategists with a lot of thought and analysis come true!",
+      social_links: useSocialLinkRoutes(),
+      imageUrl: HeroImage
+    },
+    {
+      id: '5',
+      title: "Founder & CEO",
+      name: 'Ashish Sudra',
+      description: "Lead the team of passionate developers, designers and the strategists with a lot of thought and analysis come true!",
+      social_links: useSocialLinkRoutes(),
+      imageUrl: HeroImage
+    },
+    {
+      id: '6',
+      title: "Founder & CEO",
+      name: 'Ashish Sudra',
+      description: "Lead the team of passionate developers, designers and the strategists with a lot of thought and analysis come true!",
+      social_links: useSocialLinkRoutes(),
+      imageUrl: HeroImage
+    },
+    {
+      id: '7',
+      title: "Founder & CEO",
+      name: 'Ashish Sudra',
+      description: "Lead the team of passionate developers, designers and the strategists with a lot of thought and analysis come true!",
+      social_links: useSocialLinkRoutes(),
+      imageUrl: HeroImage
+    },
+    {
+      id: '8',
+      title: "Founder & CEO",
+      name: 'Ashish Sudra',
+      description: "Lead the team of passionate developers, designers and the strategists with a lot of thought and analysis come true!",
+      social_links: useSocialLinkRoutes(),
+      imageUrl: HeroImage
+    },
+  ]
+
+console.log('82', teamMembers)
 
   return (
     <div className='w-full h-full'>
@@ -103,16 +177,120 @@ export const LandingOurTeam = () => {
 
           <div className='
             w-full
-            lg:w-[90%]
+            lg:w-[85%]
             grid
             lg:grid-cols-4
-            md:grid-cols-2
+            sm:grid-cols-2
             gap-10'>
-              {Array.from({length: 8})?.map((index, ele)=>(
-                <div className='h-[350px] bg-gray-400'>
+            {teamMembers?.map((ele, index) => (
+              <div  className='flex relative'>
+                <div key={index}
+                  className='h-[370px] relative p-3 bg-gray-400 overflow-hidden group hover:border-primary hover:border'>
+                  {/* Image Section */}
+                  <img src={ele.imageUrl} className='w-full h-full' />
 
+                  {/* Introduction section */}
+                  <div className='
+                  absolute
+                  bottom-[-20px]
+                  left-0
+                  right-0
+                  w-full
+                  h-[120px]
+                  bg-black
+                  opacity-85
+                  skew-y-[-6deg]
+                  group-hover:bg-white
+                  group-hover:opacity-100'>
+                    <div className='
+                    p-4
+                    skew-y-[6deg]
+                    w-full
+                    h-full
+                    flex
+                    flex-col
+                    items-center'>
+                      <div className='flex'>
+                        <TypographyH4 title={ele.name} className='text-white group-hover:text-black' />
+                      </div>
+                      <div className='flex'>
+                        <TypographyH5 title={ele.title} className='text-primary-foreground' />
+                      </div>
+                    </div>
+                  </div>
+
+                   {/* On hover div */}
+                  <div className='
+                  hidden
+                  absolute
+                  top-[-20px]
+                  left-0
+                  right-0
+                  bottom-[100px]
+                  skew-y-[-6deg]
+                  z-[15]
+                  opacity-95
+                  group-hover:block
+                  group-hover:bg-primary
+                 '>
+                    <motion.div className='
+                    h-full
+                    w-full
+                 
+                    flex
+                    flex-col
+                    items-center
+                    justify-center
+                    py-10
+                    px-5
+                    gap-5'
+                    initial={{y: 40,  skewY: 6}}
+                    whileInView={{y:0, skewY: 6}}
+                    transition={{duration: 0.4}}>
+                       <div className='flex text-center'>
+                        <TypographyH5 title={ele.description} className='text-white font-[400]' />
+                      </div>
+
+                      <div className='flex p-2 bg-destructive group-hover:opacity-100'>
+                        <TypographyH5 title={'Read more'} className='text-white' />
+                      </div>
+                    </motion.div>
+                  </div>
                 </div>
-              ))}
+
+                 {/* social link section */}
+                 <div className='
+                  absolute
+                  left-1/2
+                  transform -translate-x-1/2
+                  top-[94%]
+                  z-[20]
+                  group
+                  '>
+                    <div className='
+                    flex
+                    items-center
+                    justify-center
+                    gap-4
+                    bg-white
+                    border
+                    p-3
+                    px-3
+                    group-hover:border-primary'>
+                    {ele.social_links?.map(({id, icon: Icon}, index)=>(
+                          <Icon 
+                          key={index} 
+                          className="
+                          h-[15px] 
+                          text-border
+                          transition-all
+                          cursor-pointer
+                          hover:text-primary-foreground"/>
+                    ))}
+                    </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
 
@@ -123,5 +301,3 @@ export const LandingOurTeam = () => {
     </div>
   )
 }
-
-
